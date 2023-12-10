7a666d56373e592ed5793469e4b3311569aafc36
Egor Pristavka <pristavkaegor03@gmail.com>
2023-12-11 01:36:27 +0800

    Update main.py

--- auto-t-rex/main.py
+++ auto-t-rex/main.py
@@ -5,8 +5,9 @@ import cv2
 import numpy as np
 import pyautogui
 
-LOWER_JUMP_BORDER = 1100
-UPPER_JUMP_BORDER = 30000
+LOWER_JUMP_BORDER = 900
+UPPER_JUMP_BORDER = 7000
+pyautogui.PAUSE = 0.005
 
 
 def start_game():
@@ -18,19 +19,16 @@ def start_game():
 
 def process_bot_ai():
     while True:
-        shift = 0.0
-        current_screen = pyautogui.screenshot(region=(725, 220, 160, 180))
+        current_screen = pyautogui.screenshot(region=(735, 220, 160, 180))
         current_screen = cv2.cvtColor(np.array(current_screen), cv2.COLOR_RGB2BGR)
         black_pixels = np.sum(current_screen < 100)
         white_pixels = np.sum(current_screen > 100)
-        #print(black_pixels, white_pixels)
-        #cv2.imwrite(f"trex_{black_pixels}.png", current_screen)
-
+        print(black_pixels)
         if LOWER_JUMP_BORDER < black_pixels < UPPER_JUMP_BORDER:
             pyautogui.press('up')
 
-        if LOWER_JUMP_BORDER < white_pixels < UPPER_JUMP_BORDER:
-            pyautogui.press('up')
+        # if LOWER_JUMP_BORDER < white_pixels < UPPER_JUMP_BORDER:
+        #     pyautogui.press('up')
 
 
 def process_game():
